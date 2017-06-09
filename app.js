b48a41660ddcd2acfefa327f1aba0f729633fabd
Bruno Alexis Faúndez Valenzuela <gameover_sxt_nah@hotmail.com>
2017-06-09 11:01:50 -0400

    Fix cors test 7.

--- app.js
+++ app.js
@@ -18,6 +18,7 @@ var app = express();
 debug("Configuring Express app...");
 app.use(logger('dev'));
 app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Methods", "POST, PUT, OPTIONS");
   res.header("Access-Control-Allow-Origin", "http://astir.herokuapp.com/");
   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
   next();
